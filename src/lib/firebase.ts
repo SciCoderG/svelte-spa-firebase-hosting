8448d75b2b3be37587cbe08a1997deba0e663f9a
David Liebemann <david@crystalmesh.de>
2023-01-16 14:01:04 +0100

    Can now publish to firebase hosting

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAxVci0u6JRmVsIJ9-tF7607eRFJ2dYn6o",
+  authDomain: "u9-sveltekit-test-spa.firebaseapp.com",
+  projectId: "u9-sveltekit-test-spa",
+  storageBucket: "u9-sveltekit-test-spa.appspot.com",
+  messagingSenderId: "546099288262",
+  appId: "1:546099288262:web:dcf978c7bca8e643aa5fc7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
