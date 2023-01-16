6bb9a70d57e4e98629352f4099f9d16847eb4590
David Liebemann <david@crystalmesh.de>
2023-01-16 15:48:16 +0100

    Can now log in

---
+++ src/lib/Header.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+	import { getAuth, onAuthStateChanged, type Auth, type User } from 'firebase/auth';
+	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
+	import { login, logout } from './firebase';
+
+	class NavBarItem {
+		display: string;
+		route: string;
+		/**
+		 * @param {string} name
+		 * @param {string} route
+		 */
+		constructor(display: string, route: string) {
+			this.display = display;
+			this.route = route;
+		}
+	}
+
+	const navBarItems = [new NavBarItem('Home', '/')];
+
+	let user: User | null;
+	let y: number = 0;
+	$: scrollBasedClasses =
+		y > 20
+			? 'text-base-content bg-base-100 bg-opacity-90 backdrop-blur'
+			: 'text-primary-content bg-primary';
+
+	onMount(async () => {
+		const auth: Auth = getAuth();
+		onAuthStateChanged(auth, (newUser) => {
+			user = newUser;
+			console.log('On Auth State Change was called.');
+			if (null !== user) console.log(`User name: ${user?.displayName}, mail: ${user?.email}`);
+		});
+	});
+
+	function handleLogin() {
+		login();
+	}
+
+	function handleLogout(event: any) {
+		logout();
+	}
+</script>
+
+<svelte:window bind:scrollY={y} />
+
+<div
+	class="navbar fixed z-50 top-0 transition-all duration-100 shadow-md text-primary-content bg-primary"
+	transition:fade
+>
+	<div class="navbar-start" />
+	<div class="navbar-center text-xs" />
+
+	<div class="navbar-end" />
+	{#if !user}
+		<button class="btn btn-primary" on:click={handleLogin}>Anmeldung</button>
+	{:else}
+		<div class="dropdown dropdown-end text-base-content">
+			<label for="profile-dropdown" tabindex="-1" class="btn btn-ghost btn-circle avatar">
+				<div class="w-10 rounded-full">
+					<img src={user?.photoURL} />
+				</div>
+			</label>
+			<ul
+				id="profile-dropdown"
+				tabindex="-1"
+				class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+			>
+				<li class="mx-4 ">Logged in as {user?.displayName}</li>
+				<li class="mx-4 text-xs">{user?.email}</li>
+				<li><button on:click={handleLogout}>Logout</button></li>
+			</ul>
+		</div>
+	{/if}
+</div>
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,38 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, type User } from 'firebase/auth';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAxVci0u6JRmVsIJ9-tF7607eRFJ2dYn6o",
-  authDomain: "u9-sveltekit-test-spa.firebaseapp.com",
-  projectId: "u9-sveltekit-test-spa",
-  storageBucket: "u9-sveltekit-test-spa.appspot.com",
-  messagingSenderId: "546099288262",
-  appId: "1:546099288262:web:dcf978c7bca8e643aa5fc7"
+	apiKey: 'AIzaSyAxVci0u6JRmVsIJ9-tF7607eRFJ2dYn6o',
+	authDomain: 'u9-sveltekit-test-spa.firebaseapp.com',
+	projectId: 'u9-sveltekit-test-spa',
+	storageBucket: 'u9-sveltekit-test-spa.appspot.com',
+	messagingSenderId: '546099288262',
+	appId: '1:546099288262:web:dcf978c7bca8e643aa5fc7'
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+auth.useDeviceLanguage();
+const provider = new GoogleAuthProvider();
+
+export const login = async () => {
+	try {
+		const auth = getAuth(app);
+		const credential = await signInWithPopup(auth, provider);
+
+		console.log(`Logged in user: ${credential.user}`);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const logout = async () => {
+	const auth = getAuth(app);
+	await signOut(auth);
+};
