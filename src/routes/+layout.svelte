d5380fbfa6a1eb252b22ccf29fccf9736aadf52c
David Liebemann <david@crystalmesh.de>
2023-01-17 16:00:55 +0100

    Can now update and delete todos

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,14 @@
 	import '../global.css';
 </script>
 
+<svelte:head>
+	<title>U9 Todos</title>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+	/>
+</svelte:head>
+
 <Header />
 
 <body>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,20 @@
-<script>
+<script lang="ts">
 	import AddTodo from '$lib/components/AddTodo.svelte';
 	import TodoList from '$lib/components/TodoList.svelte';
+	import { user } from '$lib/stores';
 </script>
 
-<div class="grid place-items-center pt-6 gap-6 mb-6">
-	<div class="card xs:min-h-screen md:w-3/4 shadow-xl card-bordered w-full">
-		<div class="card-body">
-			<TodoList />
+{#if $user}
+	<div class="grid place-items-center pt-6 gap-6 mb-6">
+		<div class="card xs:min-h-screen md:w-3/4 shadow-xl card-bordered w-full">
+			<div class="card-body">
+				<TodoList />
+			</div>
+		</div>
+		<div class="card md:w-3/4 shadow-xl card-bordered w-full">
+			<div class="card-body">
+				<AddTodo />
+			</div>
 		</div>
 	</div>
-	<div class="card md:w-3/4 shadow-xl card-bordered w-full">
-		<div class="card-body">
-			<AddTodo />
-		</div>
-	</div>
-</div>
+{/if}
